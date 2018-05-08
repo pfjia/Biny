fea4667f0e98d92d0ab40ffe528aa3e8b6415dae
pfjia <2431631881@qq.com>
2018-05-08 20:37:14 +0800

    1.增加若干注释

--- web/index.php
+++ web/index.php
@@ -1,12 +1,14 @@
 <?php
+//默认时区配置
 date_default_timezone_set('Asia/Shanghai');
-
+// 开启debug调试模式（会输出异常）
 defined('SYS_DEBUG') or define('SYS_DEBUG', true);
+// 开启Logger页面调试
 defined('SYS_CONSOLE') or define('SYS_CONSOLE', true);
-//dev pre pub
+// dev pre pub 当前环境
 defined('SYS_ENV') or define('SYS_ENV', 'dev');
+// 系统维护中。。。
 defined('isMaintenance') or define('isMaintenance', false);
-
 if (SYS_DEBUG){
     ini_set('display_errors','On');
 }
